96f5674bbffed19f7099e4c7bd116b7bd2758e14
Marco Miranda <marcoantoniomiranda713@gmail.com>
2021-11-30 00:01:42 +0000

    Visualização da base de dados no formato simples e elegante/Visualização da documentação

--- Transformacao_Dados.R
+++ Transformacao_Dados.R
@@ -6,3 +6,23 @@ install.packages("tidyverse")
 library(nycflights13)
 library(tidyverse)
 
+# Visualizando os dados em formato simples
+nycflights13::flights
+nycflights13::airlines
+nycflights13::airports
+nycflights13::planes
+nycflights13::weather
+
+# Visualizando as documentações das bases de dados
+?flights
+?airlines
+?airports
+?planes
+?weather
+
+# Visualizando os dados de forma mais elegante
+View(flights)
+View(airlines)
+View(airports)
+View(planes)
+View(weather)
